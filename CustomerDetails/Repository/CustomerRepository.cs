468a7f7f7b778af61f30c9c5ed448e35a4071456
Danyal Saleh <danymcdano@gmail.com>
2023-11-18 17:45:12 +0000

    Final clean up

--- CustomerDetails/Repository/CustomerRepository.cs
+++ CustomerDetails/Repository/CustomerRepository.cs
@@ -7,30 +7,6 @@ namespace CustomerDetails.Repository
 {
     public class CustomerRepository : ICustomerRepository
     {
-/*        public CustomerRepository() {
-            using (var context = new CustomerDetailsContext())
-            {
-                var customers = new List<Customer>
-                {
-                    new Customer
-                    {
-                        Name = "Test",
-                        Age = 11,
-                        Height = 1.20,
-                        PostCode = "RRG 111",
-                    },
-                    new Customer
-                    {
-                        Name = "Test2",
-                        Age = 21,
-                        Height = 2.10,
-                        PostCode = "111 RRG",
-                    }
-                };
-                context.Customers.AddRange(customers);
-                context.SaveChanges();
-            }
-        }*/
 
         public async Task<List<Customer>> GetAllCustomerAsync()
         {
